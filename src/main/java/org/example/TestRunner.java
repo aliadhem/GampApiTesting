7b40a658d9f87da9ecb25e8f0d21f17b7ee915c4
ahmedabrashee <a@b.com>
2023-04-05 19:31:18 -0700

    update test 2

--- src/main/java/org/example/TestRunner.java
+++ src/main/java/org/example/TestRunner.java
@@ -3,8 +3,8 @@ package org.example;
 import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
-//    @Karate.Test
-//    Karate testGames() {
-//        return Karate.run("src/main/games.feature");
-//    }
+    @Karate.Test
+    Karate testGames() {
+        return Karate.run("src/main/games.feature");
+    }
 }
